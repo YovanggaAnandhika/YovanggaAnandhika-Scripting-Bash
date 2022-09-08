ed1ec1c3e38f6040ec2389a5f01c28e1d09cce81
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-09-09 01:29:30 +0800

    Initial commit

---
+++ src/debian/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# installing apt repository update
+sudo apt-get update
+# installing apt repository upgrade distro
+sudo apt-get upgrade
+
+# installing nginx engine
+sudo apt-get install nginx
